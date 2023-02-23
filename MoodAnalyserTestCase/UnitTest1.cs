7ec41d0cb0a25ccf0937814577574a3f8a6b3c36
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-23 14:14:54 +0530

    Test case for uc1

---
+++ MoodAnalyserTestCase/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using MoodAnalyserProject;
+
+namespace MoodAnalyserTestCase;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    [DataRow("happy", "I am in happy mood")]
+    [DataRow("sad", "I am in sad mood")]
+    public void Given_Method_Should_Return_UserMood(string expected, string message)
+    {
+        //arrange
+        //message = " I am in happy mood";
+        //expected = "happy";
+
+        MoodAnalyser mood = new MoodAnalyser(message);
+
+        //act
+        string actual = mood.AnalyseMood();
+
+        //assert
+        Assert.AreEqual(expected,actual);
+    }
+}
