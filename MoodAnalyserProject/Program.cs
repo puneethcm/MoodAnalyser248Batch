b128469b4323fc61a8cf300262d16a66c07e205e
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-28 15:30:25 +0530

    Reflection " "Reflection"

--- MoodAnalyserProject/Program.cs
+++ MoodAnalyserProject/Program.cs
@@ -1,14 +1,28 @@
-﻿using MoodAnalyserProject;
+﻿using System.Reflection;
+using MoodAnalyserProject;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Mood Analyser Project");
-        //Console.WriteLine("Enter the mood you are in : ");
-        //string message = Console.ReadLine();
-        MoodAnalyser moodAnalyser = new MoodAnalyser("");
-        Console.WriteLine(moodAnalyser.AnalyseMood());
+        ////Console.WriteLine("Enter the mood you are in : ");
+        ////string message = Console.ReadLine();
+        //MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        //Console.WriteLine(moodAnalyser.AnalyseMood());
+
+        //TestExceptionTypes.TestNullReferenceException("Kavitha");
         Console.ReadLine();
+    }
+
+    public static void TestReflection()
+    {
+        string path = "";
+        Assembly assembly = Assembly.LoadFrom(path);
+        Type[] types = assembly.GetTypes();
+        foreach(Type type in types)
+        {
+            Console.WriteLine(type.FullName);
+        }
     }
 }
